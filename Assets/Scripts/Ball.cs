ff5d786acfb015d8b189fa71657bdf1468ad101b
Ines-Fidalgo <40200308@esmad.ipp.pt>
2021-03-20 19:39:50 +0000

    Cronometro - 2 secs
    Adição de um cronometro que conta 2 segundos antes de iniciar a partida.

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    [SerializeField]
+    float velocidade = 8f;
+
+
+    bool BolaLancada;
+    float cronometro = 2.0f;
+
+    float AlturaCamara = 3f;
+    float LarguraCamara = 6.5f;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (Random.value < 0.5f)
+        {
+            GetComponent<Rigidbody2D>().velocity = velocidade * Random.insideUnitSphere;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        cronometro -= Time.deltaTime;
+
+        if (cronometro > 0.0f)
+        {
+            BolaLancada = false;
+        }
+
+        if (cronometro <= 0.0f)
+        {
+            BolaLancada = true;
+        }
+
+
+
+        if (BolaLancada == false)
+        {
+            Vector3 positionAux = transform.position;
+            positionAux.x = 0.0f;
+            positionAux.y = 0.0f;
+            positionAux.z = 0.0f;
+            transform.position = positionAux;
+        }
+
+        else
+        {
+            SegundoUpdate();
+        }
+
+    }
+    void SegundoUpdate()
+    {
+        Vector2 positionAux = transform.position;
+
+        GetComponent<Rigidbody2D>().position = positionAux;
+
+        positionAux.x = Mathf.Clamp(positionAux.x, AlturaCamara + LarguraCamara, -AlturaCamara - LarguraCamara);
+        positionAux.y = Mathf.Clamp(positionAux.y, AlturaCamara + LarguraCamara, -AlturaCamara - LarguraCamara);
+
+        // Às vezes resulta, às vezes não...  ¯\_(ツ)_/¯
+    }
+}
+
+
--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -4,13 +4,23 @@ using UnityEngine;
 
 public class Paddle : MonoBehaviour
 {
+    [SerializeField]
 
     float velocidade = 0.5f;
 
-    
+    [SerializeField]
+    KeyCode upKey = KeyCode.UpArrow;
+
+    [SerializeField]
+    KeyCode downKey = KeyCode.DownArrow;
+
+
+
     // Update is called once per frame
     void Update()
     {
+
+
         /**
          * se tecla para cima premida
          * então subir
@@ -20,15 +30,42 @@ public class Paddle : MonoBehaviour
 
 
 
-        if(Input.GetKey(KeyCode.UpArrow))
+        if (Input.GetKey(upKey))
+        {
+            // tarnsform.position += velocidade * vector3.up 
+
+            transform.position =
+                transform.position + velocidade * Vector3.up * Time.deltaTime;
+
+        }
+        else if (Input.GetKey(downKey))
         {
             transform.position =
-                transform.position + velocidade * Vector3.up;
-        } else if (Input.GetKey(KeyCode.DownArrow))
-        {
-            transform.position =
-                transform.position + velocidade * Vector3.down;
+                transform.position + velocidade * Vector3.down * Time.deltaTime;
             //Desce
+        }
+
+        float cameraHeight = Camera.main.orthographicSize;
+        float halfPaddleSize = 0.5f;
+
+        if (transform.position.y >= cameraHeight - halfPaddleSize)
+        {
+            // Na prática o códio abaixo faz isto: transform.position.y = 3f - 0.5f
+
+            Vector3 positionAux = transform.position;
+            positionAux.y = cameraHeight - halfPaddleSize;
+            transform.position = positionAux;
+        }
+
+
+        if (transform.position.y <= -cameraHeight + halfPaddleSize)
+        {
+            // Na prática o códio abaixo faz isto: transform.position.y = 3f - 0.5f
+
+            Vector3 positionAux = transform.position;
+            positionAux.y = -cameraHeight + halfPaddleSize;
+            transform.position = positionAux;
+
         }
 
     }
